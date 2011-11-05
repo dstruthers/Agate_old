bcac325c54bc155f30f61460cf6be8ea12478f69
Darren M. Struthers <dstruthers@gmail.com>
2011-11-05 10:37:17 -0700

    Committing before changing data representation of Compiled code

--- Expr.hs
+++ Expr.hs
@@ -1,84 +1,11 @@
 module Expr 
-       (
-         Expr(..)
-       , Inst(..)
-       , Env(..)
-       , Compiled
-       , fromList
-       , isPair
-       , isList
-       , isNull
-       , isTrue
-       , isFalse
-       , car
-       , cdr
-       , len
-       , Expr.parse
+       ( Expr.parse
        ) where
 
 import Data.Char (toUpper)
 import qualified Data.Map as Map
 import Text.ParserCombinators.Parsec
-
-data Inst = Argument
-          | Assign String
-          | Constant Expr
-          | Lookup String
-          | Test { consequent  :: Compiled
-                 , alternative :: Compiled
-                 }
-          deriving (Eq)
-                   
-type Compiled = [Inst]
-
-data Env = Env { runEnv    :: Map.Map String Expr
-               , parentEnv :: Maybe Env
-               } deriving (Eq)
-           
-data Expr = Symbol String
-          | Number Double
-          | String String
-          | Bool Bool
-          | Pair Expr Expr
-          | Null
-          | Procedure { params :: [String]
-                      , env :: Env
-                      , body :: Compiled
-                      }
-          | Exception String
-
-isPair :: Expr -> Bool
-isPair (Pair _ _) = True
-isPair _          = False
-
-isList :: Expr -> Bool
-isList Null        = True
-isList (Pair _ p2) = isList p2
-isList _           = False
-
-isNull :: Expr -> Bool
-isNull Null = True
-isNull _    = False
-
-isTrue :: Expr -> Bool
-isTrue = not . isFalse
-
-isFalse :: Expr -> Bool
-isFalse (Bool False) = True
-isFalse _            = False
-
-fromList :: [Expr] -> Expr
-fromList = foldr Pair Null
-
-toList :: Expr -> [Expr]
-toList (Pair x xs) = x : toList xs
-
-car (Pair x _) = x
-cdr (Pair _ y) = y
-
-len :: Expr -> Int
-len Null        = 0
-len (Pair x xs) = 1 + len xs
+import Types
 
 parseBool = do char '#'
                v <- oneOf "TFtf"
@@ -121,30 +48,3 @@ parse :: String -> Expr
 parse input = case (Text.ParserCombinators.Parsec.parse parseExpr "" input) of
   Right x -> x
   Left e -> Exception (show e)
-
-instance Show Expr where
-  show (Symbol s)   = map toUpper s
-  show (Number n)   = show n
-  show (String s)   = show s
-  show (Bool True)  = "#t"
-  show (Bool False) = "#f"
-  show p@(Pair _ _) = "(" ++ showPair p ++ ")"
-    where showPair (Pair p1 p2)
-            | isPair p2 = show p1 ++ " " ++ showPair p2
-            | isNull p2 = show p1
-            | otherwise = show p1 ++ " . " ++ show p2
-  show (Procedure _ _ _) = "#<compiled procedure>"
-  show Null = "()"
-  show (Exception s) = "**Exception: " ++ s
-
-instance Eq Expr where
-  (Symbol a) == (Symbol b) = (map toUpper a) == (map toUpper b)
-  (Number a) == (Number b) = a == b
-  (String a) == (String b) = a == b
-  (Bool a) == (Bool b) = a == b
-  (Pair a b) == (Pair c d) = a == c && b == d
-  Null == Null = True
-  (Procedure p1 e1 b1) == (Procedure p2 e2 b2) =
-    p1 == p2 && e1 == e2 && b1 == b2
-  (Exception a) == (Exception b) = a == b
-  _ == _ = False
--- VM.hs
+++ VM.hs
@@ -1,7 +1,5 @@
 module VM 
-       (
-         VM(..)
-       , compile
+       ( compile
        , execute
        , initialVM
        ) where
@@ -10,15 +8,22 @@ import Prelude hiding (lookup)
 import Control.Monad
 import Data.Char (toUpper)
 import qualified Data.Map as Map
+import Types
 import Expr
 
-data VM = VM { accumulator :: Expr
-             , environment :: Env
-             , arguments   :: [Expr]
-             }
-
-initialEnv = Env Map.empty Nothing
-initialVM = VM Null initialEnv []
+initialEnv = Env initialMap Nothing
+  where forms = [("DEFINE", SpecialForm compileDefine)
+                ,("IF", SpecialForm compileIf)
+                ,("LAMBDA", SpecialForm compileLambda)
+                ,("QUOTE", SpecialForm compileQuote)
+                ]
+        initialMap   = foldr addToMap Map.empty forms
+        addToMap (sym, binding) m = Map.insert sym binding m
+        
+initialVM = VM initialAcc initialEnv initialArgs initialStack
+  where initialAcc = Null
+        initialArgs = []
+        initialStack = Nothing
 
 setVar :: VM -> String -> Expr -> VM
 setVar vm varName val = let envMap = runEnv (environment vm)
@@ -37,18 +42,23 @@ lookup key env = let e = runEnv env
 compile :: VM -> Expr -> Compiled
 compile _ (Symbol s) = [Lookup s]
 compile vm expr
-  | isNull expr = [Constant Null]
   | isList expr == False = [Constant expr]
   | otherwise = case car expr of
-    Symbol s -> case (map toUpper s) of 
-      "DEFINE" -> compileDefine vm expr
-      "IF"     -> compileIf vm expr
-      "LAMBDA" -> compileLambda vm expr
-      "QUOTE"  -> [Constant (arg 1 expr)]
-      _        -> compilationError "unknown error"
+    Symbol s -> case lookup (map toUpper s) (environment vm) of
+      Just (SpecialForm compiler) -> compiler vm expr
+      _ -> compileApply vm expr
+    _ -> compileApply vm expr
+
+compileApply vm expr =
+  let fn = compile vm (car expr)
+  in case head fn of
+    Constant (SpecialForm compiler) -> compiler vm expr
+    _ -> compileArgs vm (cdr expr) ++ fn ++ [Apply]
+    where compileArgs vm Null = []
+          compileArgs vm (Pair arg rest) = compile vm arg ++ [Argument] ++ compileArgs vm rest
 
 compileDefine vm expr
-  | len expr /= 2 = compilationError "DEFINE requires exactly 2 arguments"
+  | len expr /= 3 = compilationError "DEFINE requires exactly 2 arguments"
   | otherwise = case (arg 1 expr) of
     Symbol sym -> let value = compile vm (arg 2 expr)
                   in value ++ [Assign sym]
@@ -77,6 +87,10 @@ compileLambda vm expr
           paramNames (Pair (Symbol p) ps) = liftM2 (:) (Just p) (paramNames ps)
           paramNames _ = Nothing
 
+compileQuote vm expr
+  | len expr > 2 = compilationError "QUOTE requires exactly 1 argument"
+  | otherwise    = [Constant (arg 1 expr)]
+
 compilationError msg = [Constant (Exception ("Compilation error: " ++ msg))]
 
 arg :: Int -> Expr -> Expr    
@@ -87,6 +101,8 @@ arg _ _ = Exception "Premature end of list, or object not a list"
 execute :: VM -> Compiled -> (Expr, VM)
 execute vm [] = (accumulator vm, vm)
 execute vm (inst:nextInst) = case inst of
+  Apply -> execute vm { accumulator = head (arguments vm) } nextInst
+  
   Argument -> let argStack = arguments vm
                   newArgs  = argStack ++ [accumulator vm]
                   vm'      = vm { arguments = newArgs }
@@ -98,11 +114,21 @@ execute vm (inst:nextInst) = case inst of
   
   Constant k -> execute vm { accumulator = k } nextInst
   
+  Frame ret -> execute vm { stack = Just (ret, vm) } nextInst
+
   Lookup s -> let s' = map toUpper s
               in case lookup s' (environment vm) of
                 Just v  -> execute vm { accumulator = v } nextInst
                 Nothing -> (Exception ("Unbound variable: " ++ s'), vm)
     
+  Return -> case stack vm of
+                 Just (next, newState) ->
+                   execute vm { environment = environment newState
+                              , arguments   = arguments newState
+                              , stack       = stack newState            
+                              } next
+                 Nothing -> (Exception "Return from empty call stack", vm)
+
   Test consequent alternative -> if isTrue (accumulator vm)
                                  then execute vm consequent
                                  else execute vm alternative
