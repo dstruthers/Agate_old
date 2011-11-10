52198e07c597828a148c58de5156b7ac08b68c90
Darren M. Struthers <dstruthers@gmail.com>
2011-11-10 14:03:39 -0800

    Improved parser a little bit

--- Parse.hs
+++ Parse.hs
@@ -3,14 +3,9 @@ module Parse
        ) where
 
 import Data.Char (toUpper)
-import qualified Data.Map as Map
 import Text.ParserCombinators.Parsec
 import Types
 
-parseBool = do char '#'
-               v <- oneOf "TFtf"
-               return $ if toUpper v == 'T' then Bool True else Bool False
-               
 parseNumber = do sign <- option "" (string "-")
                  number <- many1 digit
                  decimal <- option "0" (string "." >> many1 digit)
@@ -22,15 +17,23 @@ parseString = do char '"'
                  return $ String s
 
 parseSymbol = do f <- firstAllowed
-                 r <- many (firstAllowed <|> digit <|> oneOf "!?")
-                 return $ Symbol (f:r)
-  where firstAllowed = oneOf "+-*/" <|> letter
+                 r <- many (firstAllowed <|> digit)
+                 return $ case (map toUpper (f:r)) of 
+                   "#T" -> Bool True
+                   "#F" -> Bool False
+                   _    -> Symbol (f:r)
+  where firstAllowed = oneOf "+-*/=!@#$%^&_<>?" <|> letter
 
-parseAnyExpr = try parseBool
+parseAnyExpr = try parseSymbolWithNumber 
                <|> try parseNumber
                <|> try parseSymbol
                <|> try parseString
                <|> try parseList
+               
+-- This parser finds symbols like the incrementer 1+
+parseSymbolWithNumber = do d <- many1 digit
+                           o <- many1 (letter <|> digit <|> oneOf "+-*/=!@#$%^&_<>?")
+                           return $ Symbol (d ++ o)
 
 parseList = do char '('
                skipMany space
